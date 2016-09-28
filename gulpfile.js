d5f50b3749ffc0118c4dbefe28c8458a10eab730
Steven French <steven@frenchy.nz>
2016-09-29 09:14:50 +1300

    gulp build now works nicely with vulcanize

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,31 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const merge = require('merge-stream');
+const runSequence = require('run-sequence');
 
 var babelProcess = babel({ presets: ['es2015'] });
 
-gulp.task('elements', [], function () {
-    return gulp.src(['src/*.js'])
-            .pipe(babelProcess)
-            .pipe(gulp.dest('dist'));
+gulp.task('elements', function () {
+
+  let elementsJs = gulp.src(['src/*.js'])
+    .pipe(babelProcess)
+    .pipe(gulp.dest('dist'));
+
+  let elementsHtml = gulp.src(['src/*.html'])
+          .pipe(gulp.dest('dist'));
+
+  return merge(elementsJs, elementsHtml);
 });
 
-gulp.task('default', ['elements'], function () {});
+gulp.task('copy-root', [], function () {
+    return gulp.src(['dist/*.{html,js}'])
+            .pipe(gulp.dest('./'));
+});
+
+gulp.task('default', function (cb) {
+  runSequence(
+    'elements',
+    'copy-root',
+    cb
+  );
+});
